01922d3b90fee2b9c304850665d3a18c709daff7
Dhvani Patel <dhvanypatel7@gmail.com>
2020-05-16 03:59:58 -0400

    Changing states

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,22 @@ import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
-function Main() {
-    const todosComponent = todosData.map(item => <TodoItem key={item.id} task={item}/>)
-    return (
-        <div className="todo-list">
-            {todosComponent}
-        </div>
-    )
+class Main extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
+
+    render() {
+        const todosComponent = this.state.todos.map(item => <TodoItem key={item.id} task={item}/>)
+        return (
+            <div className="todo-list">
+                {todosComponent}
+            </div>
+        )
+    }
 }
 
 export default Main
