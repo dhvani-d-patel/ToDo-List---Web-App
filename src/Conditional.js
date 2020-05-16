03468d92b6700d92567352ef5752f5016cc6a331
Dhvani Patel <dhvanypatel7@gmail.com>
2020-05-16 19:36:16 -0400

    final logic

---
+++ src/Conditional.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Conditional(props) {
+    if(props.isLoading === true){
+        return (
+            <h1> Loading… </h1>
+        )
+    }
+
+    return (
+        <h1>Some cool stuff about conditional rendering</h1>
+    )
+}
+
+export default Conditional
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,16 @@
 import React from "react"
 
 function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cccccc",
+        textDecoration: "line-through"
+    }
+
     return (
         <div className="todo-item">
             <input type="checkbox" checked={props.task.completed} onChange={() => props.handleChange(props.task.id)}/>
-            <span>{props.task.text}</span>
+            <span style={props.task.completed ? completedStyle : null}>{props.task.text}</span>
         </div>
     )
 }
